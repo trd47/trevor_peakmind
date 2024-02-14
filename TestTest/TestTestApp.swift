23e9664e5e33e6e116eb0ed8125a4f9d9a1a66a7
Trevor Davis-Dressner <trevordsquared@gmail.com>
2024-02-13 23:41:50 -0500

    Initial Commit

---
+++ TestTest/TestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTestApp.swift
+//  TestTest
+//
+//  Created by Trevor Davis-Dressner on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
